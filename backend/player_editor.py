90fbf8620cc0512ce4a4c620899d5a9361aabedf
andreaszdw <andreaszdw@googlemail.com>
2021-05-25 19:52:15 +0200

    ui

--- backend/player_editor.py
+++ backend/player_editor.py
@@ -9,14 +9,15 @@ import tkinter as tk
 class Main(tk.Frame):
 
     def __init__(self):
-        super().__init__()
+        super().__init__(width="600", height=600)
         self.initUI()
 
     def initUI(self):
         r = 0
-        lbl_name = tk.Label(master=self, text="Name")
+        lbl_name = tk.Label(self, text="Name")
         lbl_name.grid(row=r, column=0, padx="5", pady="5")
-        ent_name = tk.Entry(master=self, text="name")
+        ent_name = tk.Entry(self)
+        ent_name.insert(0, "Name")
         ent_name.grid(row=r, column=1, padx=5, pady="5")
         r += 1
         lbl_age = tk.Label(master=self, text="Age")
