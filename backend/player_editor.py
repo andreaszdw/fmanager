e54edfd18171dc684bb2b450bd98f864f5931f6c
Andreas Grätz <andreaszdw@googlemail.com>
2021-05-26 10:17:55 +0200

    work on py interface

--- backend/player_editor.py
+++ backend/player_editor.py
@@ -10,19 +10,27 @@ class Main(tk.Frame):
 
     def __init__(self):
         super().__init__()
-        self.master.geometry("800x600")
+        # self.master.geometry("800x600")
         self.initUI()
 
     def initUI(self):
+        name="Egal Egal"
+        age=17
+        contract=2
+        salary=14000
+        playerGrid = [(name, "name"),
+                (age, "age"),
+                (contract, "contract"),
+                (salary, "salary")]
+
         r = 0
-        lbl_name = tk.Label(self, text="Name")
-        lbl_name.grid(row=r, column=0, padx="5", pady="5")
-        ent_name = tk.Entry(self)
-        ent_name.insert(0, "Name")
-        ent_name.grid(row=r, column=1, padx=5, pady="5")
-        r += 1
-        lbl_age = tk.Label(master=self, text="Age")
-        lbl_age.grid(row=r, column=0, padx="5", pady="5")
+        for l in playerGrid:
+            tmp_lbl = tk.Label(self, text=l[1])
+            tmp_lbl.grid(row=r, column=0, padx="5", pady="5")
+            tmp_ent = tk.Entry(self, width=50)
+            tmp_ent.insert(0, l[0])
+            tmp_ent.grid(row=r, column=1, padx=5, pady=5)
+            r += 1
 
         self.pack()
 
