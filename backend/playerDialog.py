f3de290bdaded3c769a7f967a40fbc758f5c862c
Andreas Grätz <andreaszdw@googlemail.com>
2021-06-10 09:15:46 +0200

    something's wrong

--- backend/playerDialog.py
+++ backend/playerDialog.py
@@ -22,31 +22,29 @@ class PlayerDialog(wx.Dialog):
 
         panel = wx.Panel(self)
 
-        static = wx.StaticBox(panel, -1, "Spielerdaten")
-        sizer = wx.GridBagSizer(5, 5)
+        gbSizer = wx.GridBagSizer(5, 5)
 
-        self.id = wx.TextCtrl(static, value=str(self.player.id))
-        self.image = wx.TextCtrl(static, value=str(self.player.imageFile))
-        self.name = wx.TextCtrl(static, value=self.player.name)
-        self.age = wx.TextCtrl(static, value=str(self.player.age))
-        self.contract = wx.TextCtrl(static, value=str(self.player.contract))
-        self.salary = wx.TextCtrl(static, value=str(self.player.salary))
-        self.foot = wx.TextCtrl(static, value=str(self.player.foot))
-        self.position = wx.TextCtrl(static, value=str(self.player.position))
-        self.fitness = wx.TextCtrl(static, value=str(self.player.fitness))
-        self.speed = wx.TextCtrl(static, value=str(self.player.speed))
-        self.stamina = wx.TextCtrl(static, value=str(self.player.stamina))
-        self.passing = wx.TextCtrl(static, value=str(self.player.passing))
-        self.header = wx.TextCtrl(static, value=str(self.player.header))
-        self.shot = wx.TextCtrl(static, value=str(self.player.shot))
-        self.tackle = wx.TextCtrl(static, value=str(self.player.tackle))
-        self.tactic = wx.TextCtrl(static, value=str(self.player.tactic))
-        self.potential = wx.TextCtrl(static, value=str(self.player.potential))
-        self.rating = wx.TextCtrl(static, value=str(self.player.rating))
-        self.exp = wx.TextCtrl(static, value=str(self.player.experience))
+        self.id = wx.TextCtrl(panel, value=str(self.player.id))
+        self.image = wx.TextCtrl(panel, value=str(self.player.imageFile))
+        self.name = wx.TextCtrl(panel, value=self.player.name)
+        self.age = wx.TextCtrl(panel, value=str(self.player.age))
+        self.contract = wx.TextCtrl(panel, value=str(self.player.contract))
+        self.salary = wx.TextCtrl(panel, value=str(self.player.salary))
+        self.foot = wx.TextCtrl(panel, value=str(self.player.foot))
+        self.position = wx.TextCtrl(panel, value=str(self.player.position))
+        self.fitness = wx.TextCtrl(panel, value=str(self.player.fitness))
+        self.speed = wx.TextCtrl(panel, value=str(self.player.speed))
+        self.stamina = wx.TextCtrl(panel, value=str(self.player.stamina))
+        self.passing = wx.TextCtrl(panel, value=str(self.player.passing))
+        self.header = wx.TextCtrl(panel, value=str(self.player.header))
+        self.shot = wx.TextCtrl(panel, value=str(self.player.shot))
+        self.tackle = wx.TextCtrl(panel, value=str(self.player.tackle))
+        self.tactic = wx.TextCtrl(panel, value=str(self.player.tactic))
+        self.potential = wx.TextCtrl(panel, value=str(self.player.potential))
+        self.rating = wx.TextCtrl(panel, value=str(self.player.rating))
+        self.exp = wx.TextCtrl(panel, value=str(self.player.experience))
 
         data = (
-            ("Spieler Daten", "head"),
             ("Spieler ID", "tc", self.id),
             ("Bild", "tc", self.image),
             ("Name", "tc", self.name),
@@ -73,32 +71,36 @@ class PlayerDialog(wx.Dialog):
         for gData in data:
             if(gData[1] == "head"):
                 tmpTxt = wx.StaticText(
-                    static, label=gData[0], style=wx.ALIGN_CENTER)
-                sizer.Add(
+                    panel, label=gData[0], style=wx.ALIGN_CENTER)
+                gbSizer.Add(
                     tmpTxt, pos=(pr, pc), span=(1, 3),
                     flag=wx.ALL | wx.EXPAND, border=15)
                 pr += 1
 
             if(gData[1] == "tc"):
-                tmpTxt = wx.StaticText(static, label=gData[0])
-                sizer.Add(
-                    tmpTxt, pos=(pr, pc), flag=wx.LEFT | wx.RIGHT, border=10)
+                tmpTxt = wx.StaticText(panel, label=gData[0])
+                gbSizer.Add(
+                    tmpTxt, pos=(pr, pc), flag=wx.LEFT | wx.RIGHT, border=15)
                 pc += 1
 
-                sizer.Add(gData[2], pos=(pr, pc), flag=wx.RIGHT, border=25)
+                gbSizer.Add(gData[2], pos=(pr, pc), flag=wx.RIGHT, border=15)
                 pr += 1
                 pc = 0
 
-        '''buttonBox = wx.BoxSizer(wx.HORIZONTAL)
+        staticBox = wx.StaticBox(panel, label="Spieler Daten")
+        staticBoxSizer = wx.StaticBoxSizer(staticBox, wx.VERTICAL)
+        staticBoxSizer.Add(gbSizer, 1, wx.EXPAND | wx.ALL, 25)
 
-        abortButton = wx.Button(panel, label="Abbrechen")
-        saveButton = wx.Button(panel, label="Speichern")
-        buttonBox.Add(abortButton, 0)
-        buttonBox.Add(saveButton, 0)
-        sizer.Add(buttonBox, pos=(pr, pc), span=(1, 3), flag=wx.RIGHT, border=10)'''
-        static.SetSizer(sizer)
-        sizer.Fit(self)
-        static.Fit()
+        # panelSizer 
+        panelSizer = wx.BoxSizer(wx.VERTICAL)
+        panelSizer.Add(staticBoxSizer, 1, wx.EXPAND | wx.ALL, 25)
+        panel.SetSizerAndFit(panelSizer)
+
+        # add the panel to the main sizer
+        mainSizer = wx.BoxSizer(wx.VERTICAL)
+        mainSizer.Add(gbSizer, proportion=1, flag=wx.ALL | wx.EXPAND, border=25)
+        self.SetSizerAndFit(mainSizer)
+        #self.Layout()
 
 class TestFrame(wx.Frame):
 
