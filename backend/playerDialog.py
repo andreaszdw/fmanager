ad73c4b92ae9510e1293eb4dc6d638b14c43d2c4
Andreas Grätz <andreaszdw@googlemail.com>
2021-06-14 12:13:44 +0200

    shit on sizers

--- backend/playerDialog.py
+++ backend/playerDialog.py
@@ -21,23 +21,47 @@ class PlayerDialog(wx.Dialog):
 
         panel = wx.Panel(self)
 
-        pr = 0
-        pc = 0
+        self.id = wx.TextCtrl(panel, value=str(self.player.id))
+        self.name = wx.TextCtrl(panel, value=self.player.name)
+        self.age = wx.TextCtrl(panel, value=str(self.player.age))
+        self.contract = wx.TextCtrl(panel, value=str(self.player.contract))
+        self.salary = wx.TextCtrl(panel, value=str(self.player.salary))
+        self.foot = wx.TextCtrl(panel, value=str(self.player.foot))
+        self.position = wx.TextCtrl(panel, value=str(self.player.position))
 
-        gbSizer = wx.GridBagSizer(5, 5)
+        mainSizer = wx.BoxSizer(wx.HORIZONTAL)
 
+        firstVBox = wx.BoxSizer(wx.VERTICAL)
+
+        # the player image
         ip = pathlib.Path.cwd() / "assets" / "player" / self.player.imageFile
-        print(ip)
 
-        pImage = wx.StaticBitmap(
+        '''pImage = wx.StaticBitmap(
             panel, bitmap=wx.Bitmap(str(ip), wx.BITMAP_TYPE_PNG))
-        gbSizer.Add(pImage, pos=(pr, pc), flag=wx.ALL, border=5)
+        firstVBox.Add(pImage, flag=wx.ALL, border=5)'''
 
-        panel.SetSizer(gbSizer)
+        data = (
+            ("Spieler ID", "tc", self.id),
+            ("Name", "tc", self.name),
+            ("Alter", "tc", self.age),
+            ("Vertrag", "tc", self.contract),
+            ("Gehalt", "tc", self.salary),
+            ("Fuss", "tc", self.foot),
+            ("Position", "tc", self.position))
 
-        mainSizer = wx.BoxSizer(wx.VERTICAL)
-        mainSizer.Add(panel, border=10)
-        self.SetSizerAndFit(mainSizer)
+        firstHBox = wx.BoxSizer(wx.HORIZONTAL)
+
+        for d in data:
+            tmpTxt = wx.StaticText(panel, label=d[0])
+            firstHBox.Add(
+                tmpTxt, flag=wx.ALL | wx.EXPAND, border=5)
+
+
+        firstVBox.Add(firstHBox, flag=wx.ALL | wx.EXPAND, border=5)
+
+
+        mainSizer.Add(firstVBox, flag=wx.EXPAND, border=10)
+        panel.SetSizerAndFit(mainSizer)
 
 class TestFrame(wx.Frame):
 
