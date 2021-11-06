fec4f5d5bb78987cb5b3201cf66f9a5b690ecdc0
andreaszdw <andreaszdw@googlemail.com>
2021-11-06 12:45:25 +0100

    work on face

---
+++ backend/faces/generator.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+
+from pathlib import Path
+from random import randint
+from player import Player
+import sqlite3
+
+class face(object):
+
+    def __init__(self, face="", hair="", brows="", eyes, nose, mouth):
+        self.face = ""
+        self.hair = ""
+        self.brows = ""
+        self.eyes = ""
+        self.nose = ""
+        self.mouth = ""
+
+def generate():
+    pass
+
+
+if __name__ == "__main__":
+
+    generate()
