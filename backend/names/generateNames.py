d6b1c2e5ddfde0a47b4d341d593a2ca92b602805
Andreas Grätz <andreaszdw@googlemail.com>
2023-04-12 08:32:31 +0200

    pep

--- backend/names/generateNames.py
+++ backend/names/generateNames.py
@@ -3,38 +3,40 @@
 from pathlib import Path
 from random import randint
 
+
 def generate_names(no, first, last, nation, save=None):
 
-        names = list()
+    names = list()
 
-        # read the file into list
-        file_fn = open(first, "r", encoding="UTF-8") # utf-8
-        f_lines = file_fn.readlines()
-        f_lines = [line.rstrip() for line in f_lines] # remove whitespace characters
-        n_f = len(f_lines) # the number of names
+    # read the file into list
+    file_fn = open(first, "r", encoding="UTF-8")  # utf-8
+    f_lines = file_fn.readlines()
+    # remove whitespace characters
+    f_lines = [line.rstrip() for line in f_lines]
+    n_f = len(f_lines)  # the number of names
 
-        file_ln = open(last, "r", encoding="UTF-8")
-        l_lines = file_ln.readlines()
-        l_lines = [line.strip() for line in l_lines]
-        n_l =  len(l_lines)
+    file_ln = open(last, "r", encoding="UTF-8")
+    l_lines = file_ln.readlines()
+    l_lines = [line.strip() for line in l_lines]
+    n_l = len(l_lines)
 
-        for no in range(0, no):
-            first = f_lines[randint(0, n_f - 1)]
-            last = l_lines[randint(0, n_l - 1)]
-            names.append(first + " " + last)
+    for no in range(0, no):
+        first = f_lines[randint(0, n_f - 1)]
+        last = l_lines[randint(0, n_l - 1)]
+        names.append(first + " " + last)
 
-        # remove duplicates
-        len(names) != len(set(names))
+    # remove duplicates
+    len(names) != len(set(names))
 
-        if save != None:
-            f = open(save, "w", encoding="UTF-8")
-            for n in names:
-                f.write(n + " - " + nation + "\n")
-            f.close()
+    if save is not None:
+        f = open(save, "w", encoding="UTF-8")
+        for n in names:
+            f.write(n + " - " + nation + "\n")
+        f.close()
 
-        print(str(len(names)) + " Namen erstellt")
+    print(str(len(names)) + " Namen erstellt")
 
-        return names
+    return names
 
 
 if __name__ == "__main__":
